f8d865110d65797a5884b87b12e6acfa69c0d6f6
mzeybek583 <muzo583@gmail.com>
2020-04-17 01:50:19 +0300

    file export logreg

--- Code/Train_Predict_LogReg_Ardanuc.R
+++ Code/Train_Predict_LogReg_Ardanuc.R
@@ -1,7 +1,6 @@
 
-## Stochastic Gradient Boosting 
-## gradient boosting machine
-#Remove everything
+# Logistic Regression
+#REmove everything
 rm(list = ls())
 # load the raster, sp, and rgdal packages
 library(raster)
@@ -59,7 +58,7 @@ rm(raster_data)
 formula <- study_area_heyelan ~ altitude + aspect + corine + curvature + dist_drenaj + dist_fay + jeoloji +  slope + twi + dist_yol
 
 ## Normalization function
-# normalize <- function(x) { return((x - min(x)) / (max(x) - min(x))) }
+normalize <- function(x) { return((x - min(x)) / (max(x) - min(x))) }
 
 #data_df_norm <- as.data.frame(lapply(data_df[], normalize))
 library(e1071) 
@@ -73,13 +72,12 @@ data_df_NA <- na.omit(data_df)
 
 ## Check NA 
 sapply(data_df_NA, function(x)sum(is.na(x)))
-
+# data_df_NA <- as.data.frame(lapply(data_df_NA, normalize))
 ## Random sampling to sampling data
 # Sample variable
 smpl <- 500
 rto <- 0.7 # Train vs Test raio
-
-## Random sampling data
+## Random sampling to 10000 data
 heyelan <- data_df_NA[data_df_NA$study_area_heyelan==1,]
 heyelan_degil <- data_df_NA[data_df_NA$study_area_heyelan==0,]
 data_df_NA_sample_heyelan <- heyelan[sample(1:nrow(heyelan), smpl, replace=FALSE),]
@@ -92,55 +90,41 @@ TrainSet <- model.data[train,]
 ValidSet <- model.data[-train,]
 summary(TrainSet)
 summary(ValidSet)
-proc.time()- time
 
 ## All subsequent models are then run in parallel
-fitControl <- trainControl(## 10-fold CV
-  method = "repeatedcv",
-  number = 10,
-  ## repeated ten times
-  repeats = 10)
+#train_control <- trainControl(method="LOOCV")
+ctrl <- trainControl(method = "repeatedcv", number = 10, savePredictions = TRUE)
 
 # Create Model ------------------------------------------------------------
 
-model_gbm <- train(formula, data = TrainSet, 
-                 method = "gbm", 
-                 trControl = fitControl,
-                 ## This last option is actually one
-                 ## for gbm() that passes through
-                 verbose = FALSE, preProc = c("center", "scale"))
-proc.time()- time
+model_glm <- train(formula, data = TrainSet, 
+                   trControl = ctrl, method = "glm",
+                   family = "binomial",preProc = c("center", "scale")) 
 
-model_gbm
+proc.time() -time
+model_glm
 
-## GBM Model
+names(model_glm)
+model_glm$results
+summary(model_glm)
 
-names(model_gbm)
-model_gbm$results
-summary(model_gbm)
+gbmImp <- varImp(model_glm, scale = TRUE)
+gbmImp
+saveRDS(gbmImp,"RESULT/Logreg_Train_varimportance")
+#readRDS("Logreg_Train_varimportance")
 
-#gbmImp <- varImp(model_gbm, scale = TRUE)
-#gbmImp
-#saveRDS(gbmImp,"RESULT/GBM_Train_varimportance")
-#trellis.par.set(caretTheme())
-#plot(model_gbm) 
-
-#readRDS("GBM_Train_varimportance")
-
-#png("TRAIN_varImportance_GBM.png")
-#tiff("RESULT/TRAIN_varImportance_GBM.tiff", units="cm", width=8, height=8, res=600)
-#plot(gbmImp, top = 10)
-#dev.off()
+#png("TRAIN_varImportance_LogReg.png")
+tiff("RESULT/TRAIN_varImportance_LogReg.tiff", units="cm", width=8, height=8, res=600)
+plot(gbmImp, top = 10)
+dev.off()
 
 # Do not change
-saveRDS(model_gbm, "RESULT/super_model_GBM")
+saveRDS(model_glm, "RESULT/super_model_LogReg")
 # Do not change
 
 library(gmodels)
-pred_train <-predict(model_gbm, TrainSet[,-13])
-pred_valid <-predict(model_gbm, ValidSet[,-13])
-
-# ROC plots ---------------------------------------------------------------
+pred_train <-predict(model_glm, TrainSet[,-13])
+pred_valid <-predict(model_glm, ValidSet[,-13])
 
 library(ROCR)
 pred <- prediction(predictions = pred_train, labels = TrainSet$study_area_heyelan)
@@ -148,11 +132,11 @@ pred_valid <- prediction(predictions = pred_valid, labels = ValidSet$study_area_
 
 perf <- performance(pred, measure = "tpr", x.measure = "fpr")
 perf_valid <- performance(pred_valid, measure = "tpr", x.measure = "fpr")
-saveRDS(perf_valid,"RESULT/TRAIN_GBM_validation_ROC")
-#aa <- readRDS("GBM_validation_ROC")
-#png("TRAIN_roc_curve_train_GBM.png")
-tiff("RESULT/TRAIN_roc_curve_train_GBM.tiff", units="cm", width=8, height=8, res=600)
-plot(perf, main = "ROC curve for Landslide Detection Train Data (GBM)", col = "blue", lwd = 3)
+saveRDS(perf_valid,"RESULT/TRAIN_Logreg_validation_ROC")
+#aa <- readRDS("Logreg_validation_ROC")
+#png("TRAIN_roc_curve_train_LogReg.png")
+tiff("RESULT/TRAIN_roc_curve_train_LogReg.tiff", units="cm", width=8, height=8, res=600)
+plot(perf, main = "ROC curve for Landslide Detection Train Data (LogReg)", col = "blue", lwd = 3)
 abline(a = 0, b = 1, lwd = 2, lty = 2)
 dev.off()
 
@@ -160,12 +144,12 @@ perf.auc <- performance(pred, measure = "auc")
 str(perf.auc)
 unlist(perf.auc@y.values)
 ## Export accuracy
-dput(perf.auc, "RESULT/TRAIN_GBM.txt")
+dput(perf.auc, "RESULT/TRAIN_LogReg.txt")
 
-#png("TRAIN_roc_curve_valid_GBM.png")
-tiff("RESULT/TRAIN_roc_curve_valid_GBM.tiff", units="cm", width=8, height=8, res=600)
+#png("TRAIN_roc_curve_valid_LogReg.png")
+tiff("RESULT/TRAIN_roc_curve_valid_LogReg.tiff", units="cm", width=8, height=8, res=600)
 
-plot(perf_valid, main = "ROC curve for Landslide Detection Validation Data (GBM)", col = "blue", lwd = 3)
+plot(perf_valid, main = "ROC curve for Landslide Detection Validation Data (LogReg)", col = "blue", lwd = 3)
 abline(a = 0, b = 1, lwd = 2, lty = 2)
 dev.off()
 
@@ -174,14 +158,15 @@ perf.auc_valid <- performance(pred_valid, measure = "auc")
 str(perf.auc_valid)
 unlist(perf.auc_valid@y.values)
 ## Export accuracy
-dput(perf.auc_valid, "RESULT/TRAIN_valid_GBM.txt")
+dput(perf.auc_valid, "RESULT/TRAIN_valid_LogReg.txt")
 
 # Predict raster with produced Super Model --------------------------------
 ## Apply to raster prediction
 raster_data <- stack(altitude, aspect, corine, curvature, drenaj, fay, jeoloji, slope, twi, yol, cls)
 names(raster_data)
-r1 <- raster::predict(raster_data, model_gbm, progress="text")
+r1 <- raster::predict(raster_data, model_glm, progress="text")
 plot(r1)
 
-writeRaster(r1,"RESULT/TRAIN_GBM.tif", overwrite=TRUE)
+writeRaster(r1,"RESULT/TRAIN_LogReg.tif", overwrite=TRUE)
+
 proc.time() - time
