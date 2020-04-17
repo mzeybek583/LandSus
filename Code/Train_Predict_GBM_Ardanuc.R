c83c0e8d45cce81f68b308c267fcd7a4a522536b
mzeybek583 <muzo583@gmail.com>
2020-04-17 03:52:23 +0300

    working from single file and add to export file

--- Code/Train_Predict_GBM_Ardanuc.R
+++ Code/Train_Predict_GBM_Ardanuc.R
@@ -8,54 +8,39 @@ library(raster)
 library(sp)
 library(rgdal)
 library(caret) 
+
 #library(doParallel)
-
 #registerDoParallel(cores = 3)
+
 set.seed(917);   
-
-#setwd("pathToDirHere") # set the working directory to the data
-
-# time 
-time <- proc.time()
+time <- proc.time()# time 
 
 # DATA --------------------------------------------------------------------
-# import raw data tiffs
-altitude <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/R_SVM/altitude.tif"
-aspect <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/R_SVM/aspect.tif"
-corine <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/R_SVM/corine.tif"
-curvature <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/R_SVM/curvature.tif"
-drenaj <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/R_SVM/dist_drenaj.tif"
-fay <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/R_SVM/dist_fay.tif"
-jeoloji <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/R_SVM/jeoloji.tif"
-slope <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/R_SVM/slope.tif"
-yol <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/R_SVM/dist_yol.tif"
-twi <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/R_SVM/twi.tif" 
-#twi <- "../Data/R_SVM/twi.tif"
+# change
+Tiff_path <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/R_SVM/" 
+Working_path <- "/home/mzeybek/LandSus/Code/" # change
+smpl <- 500 # Sample variable
+rto <- 0.7 # Train vs Test raio
 
-# Landslide Ground Truth DATA
+setwd(Tiff_path)
 
-cls <- "/media/mzeybek/7C6879566879105E/LandslideSusceptibility/Data/study_area_heyelan/study_area_heyelan.tif"
-
-# Save Exports to RESULT folder --------------------------------------------------
-mainDir <- "/home/mzeybek/LandSus/Code/"
-subDir <- "RESULT"
-
-dir.create(file.path(mainDir, subDir), showWarnings = FALSE)
-setwd(file.path(mainDir))
-
-# create raster stack
-raster_data <- stack(altitude, aspect, corine, curvature, drenaj, fay, jeoloji, slope, twi, yol, cls)
+# Save Exports to RESULT folder
+Result_Dir <- "RESULT" # Output file # Do not change
+temp = list.files(path = Tiff_path , pattern="\\.tif$", 
+                  full.names = FALSE, recursive = TRUE)
+raster_data <- raster::stack(paste0(temp))
+dir.create(file.path(Working_path, Result_Dir), showWarnings = FALSE)
 
 # check attributes
 raster_data
 proc.time()- time
 names(raster_data)
 data_df <- as.data.frame(raster_data, xy=TRUE)
-rm(raster_data)
 
 # Train Formula -----------------------------------------------------------
 
-formula <- study_area_heyelan ~ altitude + aspect + corine + curvature + dist_drenaj + dist_fay + jeoloji +  slope + twi + dist_yol
+formula <- study_area_heyelan ~ altitude + aspect + corine + 
+  curvature + dist_drenaj + dist_fay + jeoloji +  slope + twi + dist_yol
 
 ## Normalization function
 # normalize <- function(x) { return((x - min(x)) / (max(x) - min(x))) }
@@ -73,16 +58,12 @@ data_df_NA <- na.omit(data_df)
 ## Check NA 
 sapply(data_df_NA, function(x)sum(is.na(x)))
 
-## Random sampling to sampling data
-# Sample variable
-smpl <- 500
-rto <- 0.7 # Train vs Test raio
-
 ## Random sampling data
 heyelan <- data_df_NA[data_df_NA$study_area_heyelan==1,]
 heyelan_degil <- data_df_NA[data_df_NA$study_area_heyelan==0,]
 data_df_NA_sample_heyelan <- heyelan[sample(1:nrow(heyelan), smpl, replace=FALSE),]
-data_df_NA_sample_heyelan_degil <- heyelan_degil[sample(1:nrow(heyelan_degil), smpl, replace=FALSE),]
+data_df_NA_sample_heyelan_degil <- heyelan_degil[sample(1:nrow(heyelan_degil), 
+                                                        smpl, replace=FALSE),]
 
 model.data <- rbind(data_df_NA_sample_heyelan, data_df_NA_sample_heyelan_degil)
 #split data into a train and test set 
@@ -131,19 +112,22 @@ summary(model_gbm)
 #plot(gbmImp, top = 10)
 #dev.off()
 
+setwd(file.path(Working_path))
+
 # Do not change
 saveRDS(model_gbm, "RESULT/super_model_GBM")
 # Do not change
 
 library(gmodels)
-pred_train <-predict(model_gbm, TrainSet[,-13])
-pred_valid <-predict(model_gbm, ValidSet[,-13])
+pred_train <-predict(model_gbm, TrainSet[,-12])
+pred_valid <-predict(model_gbm, ValidSet[,-12])
 
 # ROC plots ---------------------------------------------------------------
 
 library(ROCR)
 pred <- prediction(predictions = pred_train, labels = TrainSet$study_area_heyelan)
-pred_valid <- prediction(predictions = pred_valid, labels = ValidSet$study_area_heyelan)
+pred_valid <- prediction(predictions = pred_valid, 
+                         labels = ValidSet$study_area_heyelan)
 
 perf <- performance(pred, measure = "tpr", x.measure = "fpr")
 perf_valid <- performance(pred_valid, measure = "tpr", x.measure = "fpr")
@@ -151,7 +135,8 @@ saveRDS(perf_valid,"RESULT/ROC_Curve_valid_GBM")
 #aa <- readRDS("GBM_validation_ROC")
 #png("TRAIN_roc_curve_train_GBM.png")
 tiff("RESULT/ROC_Curve_train_GBM.tiff", units="cm", width=8, height=8, res=600)
-plot(perf, main = "ROC curve for Landslide Detection Train Data (GBM)", col = "blue", lwd = 3)
+plot(perf, main = "ROC curve for Landslide Detection Train Data (GBM)", 
+     col = "blue", lwd = 3)
 abline(a = 0, b = 1, lwd = 2, lty = 2)
 dev.off()
 
@@ -164,7 +149,8 @@ dput(perf.auc, "RESULT/Perf_AUC_train_GBM.txt")
 #png("TRAIN_roc_curve_valid_GBM.png")
 tiff("RESULT/ROC_Curve_valid_GBM.tiff", units="cm", width=8, height=8, res=600)
 
-plot(perf_valid, main = "ROC curve for Landslide Detection Validation Data (GBM)", col = "blue", lwd = 3)
+plot(perf_valid, main = "ROC curve for Landslide Detection Validation Data (GBM)", 
+     col = "blue", lwd = 3)
 abline(a = 0, b = 1, lwd = 2, lty = 2)
 dev.off()
 
@@ -176,8 +162,6 @@ dput(perf.auc_valid, "RESULT/Perf_AUC_GBM.txt")
 
 # Predict raster with produced Super Model --------------------------------
 ## Apply to raster prediction
-raster_data <- stack(altitude, aspect, corine, curvature, drenaj, fay, jeoloji, slope, twi, yol, cls)
-names(raster_data)
 r1 <- raster::predict(raster_data, model_gbm, progress="text")
 plot(r1)
 
