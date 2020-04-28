5f8da36be1e006504569a493a12958b3ef60295c
mzeybek583 <muzo583@gmail.com>
2020-04-28 21:47:51 +0300

    AUC text added

--- Code/ROC_plots_1.R
+++ Code/ROC_plots_1.R
@@ -10,19 +10,25 @@ setwd(dir = "/home/mzeybek/LandSus/Code/")
 
 # Load ROC data -----------------------------------------------------------
 
-model_1 <- readRDS("TRAIN_Logreg_validation_ROC")
-model_2 <- readRDS("ROC_Curve_valid_SVM")
-
+model_1 <- readRDS("../Code/RESULT/TRAIN_Logreg_validation_ROC")
+model_2 <- readRDS("../Code/RESULT/ROC_Curve_valid_SVM")
+model_3 <- readRDS("../Code/RESULT/ROC_Curve_valid_RF")
 
 # Plot ROC models ---------------------------------------------------------
 
 png(filename = "../Code/RESULT/ROC_Result.png", width = 400, height = 400)
 par(pty="s",cex.axis=1.5, cex.lab=1.3)
 plot(model_1, col="red", lwd=1.5, cex.axis=1.5)
+text(0.9, 0.5, labels ="AUC: 0.87", col="red")
+
 plot(model_2, add = TRUE, col = "green", lwd=1.5)
+text(0.9, 0.4, labels ="AUC: 0.87", col="green")
+
+plot(model_3, add = TRUE, col = "blue", lwd=1.5)
+text(0.9, 0.3, labels ="AUC: 0.87", col="blue")
 legend(x = "bottomright", 
-       legend = c("GLM", "SVM"),
-       col = c("red","green"), lty=1, lwd=1.5, cex=1)
+       legend = c("GLM", "SVM", "RF"),
+       col = c("red","green", "blue"), lty=1, lwd=1.5, cex=1)
 abline(a = 0, b = 1, lwd = 1.5, lty = 2)
 
 dev.off()
