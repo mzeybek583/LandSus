c08cdec0b3a3f098b7db3a7ccf01c6e891add5a9
mzeybek583 <muzo583@gmail.com>
2020-04-28 21:04:29 +0300

    small changes

--- Code/ROC_plots_1.R
+++ Code/ROC_plots_1.R
@@ -6,11 +6,9 @@
 library(ROCR)
 
 # Working Directory
-setwd(dir = "Code/RESULT/")
-
+setwd(dir = "/home/mzeybek/LandSus/Code/")
 
 # Load ROC data -----------------------------------------------------------
-
 
 model_1 <- readRDS("TRAIN_Logreg_validation_ROC")
 model_2 <- readRDS("ROC_Curve_valid_SVM")
@@ -18,15 +16,13 @@ model_2 <- readRDS("ROC_Curve_valid_SVM")
 
 # Plot ROC models ---------------------------------------------------------
 
-png(filename = "../RESULT/ROC_Result.png", width = 400, height = 400)
+png(filename = "../Code/RESULT/ROC_Result.png", width = 400, height = 400)
 par(pty="s",cex.axis=1.5, cex.lab=1.3)
-
 plot(model_1, col="red", lwd=1.5, cex.axis=1.5)
 plot(model_2, add = TRUE, col = "green", lwd=1.5)
 legend(x = "bottomright", 
        legend = c("GLM", "SVM"),
        col = c("red","green"), lty=1, lwd=1.5, cex=1)
 abline(a = 0, b = 1, lwd = 1.5, lty = 2)
-
 
 dev.off()
